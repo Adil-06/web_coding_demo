a48e350618c7c5d319fddb471cc5d1acdf022f47
Adil-06 <56430701+Adil-06@users.noreply.github.com>
2019-10-11 19:00:17 +0500

    js object practice
    js objects , constructor function, methods, accessors

---
+++ js_para/object-demo.js
@@ -0,0 +1,40 @@
+
+ var person = { fName:"alex",
+             lName :"hale",
+            gender:"male",
+            age :"30",
+            get fullname (){ 
+                var name = this.fName + "  " + this.lName;
+                return  name.toUpperCase();
+
+            }
+    } ;
+
+var update_obj = person;
+update_obj.age = 20;
+// the use of getter method
+document.getElementById("fn").innerHTML = person.fullname;
+
+// for accessing obj property
+document.getElementById("info_demo").innerHTML = "the name is = " +person.fName +"  " + "and " + "age is = " +person.age;
+
+// adding new property in object
+person.nationality = "english";
+
+// for in loop 
+var x ;
+var res = "";
+for (x in person) {
+   res += person[x] + "<br>";
+}
+
+document.getElementById("full_info").innerHTML = res.toUpperCase();
+
+// for constructor function
+/* function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  } */
+  
