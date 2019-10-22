518645d7d63e5275c019a2ad9e9b725748627b5f
Adil-06 <56430701+Adil-06@users.noreply.github.com>
2019-10-22 13:36:26 +0500

    js simple calculator

--- js-exercise/exer-cal.js
+++ js-exercise/exer-cal.js
@@ -1,18 +1,26 @@
 
 
-var result , clear, add, min, divide, mutli, firstNum , secondNum ;
+var input = document.getElementById("inputVal");
+// to show num
+  function showNum(obj) {
+     // console.log("inside shownum");
+    var store = obj.innerHTML;
+    if (store == '=') {
+       // console.log('1st if');
+        input.innerHTML = eval(input.innerHTML);
+    } else if (store == 'C') {
+        input.innerHTML = '0';
 
- function getNum4() {
-   var x = document.getElementById("num4").innerHTML;
-   return x;
-};
-var show = function () {
- //  console.log("here");
-    document.forms["myform"]["fn"].value = getNum4();
-};
- function clearAll() { 
-     console.log("clear");
- document.forms['myform']['fn'].value = "0";
+    } 
+    else {
+        if (input.innerHTML == '0') {
+          //  console.log("2nd if");
+            input.innerHTML = store;
+        } else {
+            input.innerHTML = input.innerHTML + store;
+        }
+    }
+}
 
-};
+
 
