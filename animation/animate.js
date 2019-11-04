8ac16955335632318f55a6054af6eb7c3b5b38be
Adil-06 <56430701+Adil-06@users.noreply.github.com>
2019-11-04 18:26:35 +0500

    animation error corrections

--- animation/animate.js
+++ animation/animate.js
@@ -16,35 +16,35 @@ $(document).ready(function(){
     
     function move()
     { 
-        $(".img1").css({ "width":"15px" , "height" : "25px" }).slideUp(2000).slideDown(2000);
-        $(".img2").css({ "width":"15px" , "height" : "25px" }).slideUp(2000).slideDown(2000);
-        $(".img3").css({ "width":"15px" , "height" : "25px" }).slideUp(2000).slideDown(2000);
-        $(".img4").css({ "width":"15px" , "height" : "25px" }).slideUp(2000).slideDown(2000);
-        $(".img5").css({ "width":"15px" , "height" : "25px" }).slideUp(2000).slideDown(2000);
+        $(".img1").css({ "width":"8px" , "height" : "12px" }).slideUp(1000).slideDown(500);
+        $(".img2").css({ "width":"10px" , "height" : "14px" }).slideUp(1000).slideDown(500);
+        $(".img3").css({ "width":"12px" , "height" : "16px" }).slideUp(1000).slideDown(500);
+        $(".img4").css({ "width":"14px" , "height" : "18px" }).slideUp(1000).slideDown(500);
+        $(".img5").css({ "width":"16px" , "height" : "20px" }).slideUp(1000).slideDown(500);
     }
        
-    var startani =  setInterval(move,5000); 
-    // to start animaton
-    // $(".btnstart").click(function()
-    // {
-    //    setInterval(move); 
+    var startani =  setInterval(move,2000); 
+      
+     // to stop animation
+    //  $(".btnstop").click(function(){
+    //      console.log("inside stop");
+         
+    //      clearInterval(startani);
+
+    //  });
+
+     var stop = document.getElementById("stop");
+     stop.addEventListener("click",stopani);
+     function stopani(){
+         clearInterval(startani);
+         resizeimg();
+     }
+     //stop.addEventListener("click", resizeimg);
+     function resizeimg(){
+   $(".img1 ,.img2, .img3, .img4, .img5").css({"width":"15px" , "height":"25px"});
+
+     }
+
+
     
-    // });
-    
-
-    // to stop animation
-    $(".btnstop").click(function(){
-        console.log("inside stop");
-   //  $(".img1 .img2 .img3 .img4 .img5").css({"width":"10px" , "height":"20px"});
-       
-       clearInterval(startani);
-
-  
-
-    });
-
-
-
-
-
 });
