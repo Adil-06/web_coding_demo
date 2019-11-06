5975ff41681bfb37c83452c96995ad05e99cf969
Adil-06 <56430701+Adil-06@users.noreply.github.com>
2019-11-06 17:37:20 +0500

    addition of new sliding text

--- slider-task/slide.js
+++ slider-task/slide.js
@@ -12,17 +12,50 @@ $(document).ready(function()
        $('.slide-text').next().fadeOut(1000);
 
     } ,5000);
-    // gettext[0].html ="text";
+    
     });
 
-// $(function(){
+ // $(function(){ // for autoslide
     
-//     $('#ul .slide-text:gt(0)').hide();
-//     setInterval(function(){
-//       $('#ul :first-child').fadeOut(1000).next('.slide-text').fadeIn(2000)
-//       .end().appendTo('#ul');
-//   }, 5000);
+ //     $('#ul .slide-text:gt(0)').hide();
+ //     setInterval(function(){
+ //       $('#ul :first-child').fadeOut(1000).next('.slide-text').fadeIn(2000)
+ //       .end().appendTo('#ul');
+ //   }, 5000);
     
-//   });
- 
+ //   });
+
+ // second slider code
+   $(".btn-move").click(function()
+   {
+
+       console.log("in move-btn");
+       $('.second-slider .demo-slider:gt(0)').hide();
+       setInterval(function(){
+           $('.second-slider :first-child').fadeOut(100).next('.demo-slider')
+           .fadeIn(2000).end().appendTo('.second-slider');
+
+       },4000);
+
+     //  $(".demo-slider").fadeOut("slow");
+    //  $(".demo-slider").each(function(index){
+    //      if(index == 0){
+    //      $(this).fadeOut(1000);
+    //      }
+    //      if(index == 1){
+    //          $(this).fadeOut(2000);
+    //      }
+    //      if(index == 2){
+    //          $(this).fadeOut(3000)
+    //      }
+    //      else {
+    //          $(this).fadeIn(1000);
+    //      }
+    //  });
+
+
+
+    });
+
+
 });
