7a0d2db2706954560a5b456110f80610d26195ab
Adil-06 <56430701+Adil-06@users.noreply.github.com>
2019-12-06 15:25:41 +0500

    javascript slider

--- javascripte-slider/slider.js
+++ javascripte-slider/slider.js
@@ -1,43 +1,46 @@
 
-// var imgslide = setInterval(SlideImage, 4000);
-//  to slide images
-//   function SlideImage() {
-//   if (curPos == cntImages - 1)
-//   curPos = 0;
-//   else
-//   curPos++;
-//   $('#sliderWrapper').animate({
-//   'marginLeft': sliderWidth * (-curPos)
-//   },2000);
-//  }
+(function(){
 
-var currentpos = 0;
-var totalimg = document.getElementsByClassName("slider");
-var timgLen = totalimg.length;
+    // required variables
+    var currentpos = 0;
+    var totalimg = document.getElementsByClassName("slider");
+    var timgLen = totalimg.length;
 
-// to get the total width
-for (var w = 0; w < totalimg.length; w++) {
-    var sliderWidth = totalimg[w].clientWidth;
-}
-// console.log(sliderWidth);
-    var totalwidth = sliderWidth *timgLen;
-console.log("the  width is = "+totalwidth);
+  // to get the total width
+  for (var w = 0; w <= totalimg.length-1; w++) {
+      var sliderWidth = totalimg[w].clientWidth;
+    }
 
-var sliderdiv = document.getElementById("sliderWrapper");
-sliderdiv.style.width = totalwidth +'px';
- 
-//var imgslide = setInterval(slideimage, 4000);
+  //setting and getting width of slider wrapper
+  var totalwidth = sliderWidth *timgLen;
+  console.log("the  width is = "+totalwidth+"px");
+  var sliderdiv = document.getElementById("sliderWrapper");
+  sliderdiv.style.width = totalwidth +'px';
 
- function slideimage () {
-  
- if(currentpos == timgLen-1){
-      currentpos = 0;
- } else {
-     
-     currentpos ++;
-     //sliderdiv.style.animationName = "move";
-   
-     sliderdiv.style.marginLeft = sliderWidth*(-currentpos)+'px';
- }
+ // to start slideshow
+  var imgslide = setInterval(slideimage, 5000);
 
- }
+ function slideimage ()
+  {
+      if(currentpos == timgLen-1) {
+          currentpos = 0;
+        } else {
+            var first = sliderdiv.firstChild;
+            currentpos ++;
+            sliderdiv.style.marginLeft = sliderWidth*(-currentpos)+'px';
+            sliderdiv.style.transition = '5s';
+            sliderdiv.appendChild( first);
+        }
+    }
+
+  // to stop start
+     var startStop = document.getElementById("btnstop-start");
+     startStop.addEventListener("click" , stopSliding);
+     function stopSliding () {
+         console.log("images stop");
+         clearInterval(imgslide);
+     }
+
+
+//end
+ })();
